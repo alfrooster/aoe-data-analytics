17f0b88a54af3fc5ac4d61b6fab6f9c7afec7623
Juuso Ihatsu <juuso.ihatsu@gmail.com>
2023-03-30 15:31:48 +0300

    two new templates for different functionalities and more

--- flask/analysis.py
+++ flask/analysis.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-def analyse():
+df = pd.read_csv("./aoe2.csv")
 
-    df = pd.read_csv("./aoe2.csv")
+def formulate_strat(selected_map, selected_player_civ, selected_enemy_civ):
 
     # WILL BE REPLACED BY USER INPUT
     #ask user for elo
@@ -14,16 +14,33 @@ def analyse():
 
     #filter to games where elo is higher than user input
     table_high = df.loc[df['rating.win'] >= elo]
-    listmaps = []
-    for i in df['map_type.name']:
-        if i not in listmaps:
-            listmaps.append(i)
-    #print(sorted(listmaps))
     
     #filter to games in chosen map
     map_table_high = table_high.loc[df['map_type.name']  == map]
     #filter by duration
     map_table_high = map_table_high.loc[map_table_high['duration'] < dura]
+
+    #wins with civ / all games with civ
+    map_winrates_high = map_table_high['civ.win.name'].value_counts()/(map_table_high['civ.win.name'].value_counts()+map_table_high['civ.lose.name'].value_counts())
+    #sort winrates and name columns
+    map_winrates_high = map_winrates_high.sort_values(ascending = False).reset_index().rename(columns={'index':'civs',0:'win_rate'})
+    #winrate to percentage
+    map_winrates_high.win_rate = map_winrates_high.win_rate * 100
+    #index starts from 1
+    map_winrates_high.index += 1
+
+    #print top 5 civs
+    return map_winrates_high.head(5).to_html()
+
+def analyze_winrates(selected_elo, selected_map, selected_duration):
+
+    #filter to games where elo is higher than user input
+    table_high = df.loc[df['rating.win'] >= int(selected_elo)]
+    
+    #filter to games in chosen map
+    map_table_high = table_high.loc[df['map_type.name']  == selected_map]
+    #filter by duration
+    map_table_high = map_table_high.loc[map_table_high['duration'] < selected_duration]
 
     #wins with civ / all games with civ
     map_winrates_high = map_table_high['civ.win.name'].value_counts()/(map_table_high['civ.win.name'].value_counts()+map_table_high['civ.lose.name'].value_counts())
--- flask/app.py
+++ flask/app.py
@@ -11,21 +11,51 @@ def index():
     
     return render_template('index.html', map_list=map_list, civ_name_list=civ_name_list)
 
+@app.route('/winrates', methods=['GET', 'POST'])
+def winrates():
+
+    map_list = ['Acropolis', 'Arabia', 'Arena', 'Atacama', 'Fortress', 'Four Lakes', 'Golden Pit', 'Hideout', 'Islands', 'MegaRandom', 'Migration', 'Nomad']
+
+    return render_template('winrates.html', map_list=map_list)
+
+@app.route('/stratoptimizer', methods=['GET', 'POST'])
+def stratoptimizer():
+
+    map_list = ['Acropolis', 'Arabia', 'Arena', 'Atacama', 'Fortress', 'Four Lakes', 'Golden Pit', 'Hideout', 'Islands', 'MegaRandom', 'Migration', 'Nomad']
+    civ_name_list = ['Aztecs', 'Berbers', 'Britons', 'Bulgarians', 'Burgundians', 'Burmese', 'Byzantines', 'Celts', 'Chinese', 'Cumans', 'Ethiopians', 'Franks', 'Goths', 'Huns', 'Incas', 'Indians', 'Italians', 'Japanese', 'Khmer', 'Koreans', 'Lithuanians', 'Magyars', 'Malay', 'Malians', 'Mayans', 'Mongols', 'Persians', 'Portuguese', 'Saracens', 'Sicilians', 'Slavs', 'Spanish', 'Tatars', 'Teutons', 'Turks', 'Vietnamese', 'Vikings']
+    
+    return render_template('stratoptimizer.html', map_list=map_list, civ_name_list=civ_name_list)
+
+
 @app.route('/results', methods=['GET', 'POST'])
 def results():
-    if request.method == 'POST':
+    if request.method == 'POST' and request.form.get('data_from_where') == 'stratoptimizer':
         selected_map = request.form.get('aoe2map')
         selected_player_civ = request.form.get('player_civ_name')
         selected_enemy_civ = request.form.get('enemy_civ_name')
 
+        print(request.form.get('data_from_where'))
         print(selected_map)
         print(selected_player_civ)
         print(selected_enemy_civ)
     
-    res = analysis.analyse()
-    #print(res)
+        res = analysis.formulate_strat(selected_map, selected_player_civ, selected_enemy_civ)
+        #print(res)
+        return render_template('results.html', res=res)
 
-    return render_template('results.html', res=res)
+    elif request.method == 'POST' and request.form.get('data_from_where') == 'winrates':
+        selected_elo = request.form.get('min_elo')
+        selected_map = request.form.get('aoe2map')
+        selected_duration = request.form.get('duration')
+
+        print(request.form.get('data_from_where'))
+        print(selected_elo)
+        print(selected_map)
+        print(selected_duration)
+
+        res = analysis.analyze_winrates(selected_elo, selected_map, selected_duration)
+
+        return render_template('results.html', res=res)
 
 
 if __name__ == "__main__":
